d5116f6fac82ef78ad41274cb7ce8acd4bbe58a4
RonF25 <57496986+RonF25@users.noreply.github.com>
2020-04-02 14:37:43 -0400

    sdfgh

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ console.log("Connected");
     pets: ["cat", "dog"]
 }; */
 
-const Haoran = {
+/* const Haoran = {
     name: "how",
     dob: 2001,
     year: "Senior",
@@ -31,14 +31,39 @@ const Haoran = {
         //console.log(this);
         return 2020 - this.dob;
     }
-};
+}; */
 
 /*const Eric = Haoran; /// refrence to Haoran DONT USE THIS
 Eric.name = "Eric" /// This makes Haorans name Eric
 const Christian = {};
 Christian.dob = "Haoran.dob" /// DO THIS
 Christian.dob = 2003;*/
-const calc = {
+/* const calc = {
     add: ///function x + y,
     subtract: /// function x - y
-}
+} */
+const presidents = [
+    { name: "Donald Trump", terms: 1, party: "R" },
+    { name: "Barack Obama", terms: 2, party: "D" },
+    { name: "George W. Bush", terms: 2, party: "R" },
+    { name: "Bill Clinton", terms: 2, party: "D" },
+    { name: "George H.W. Bush", terms: 1, party: "R" },
+    { name: "Ronald Reagan", terms: 2, party: "R" },
+    { name: "Jimmy Carter", terms: 1, party: "D" },
+    { name: "Gerlad Ford", terms: 1, party: "R" },
+    { name: "Richard Nixon", terms: 2, party: "R" },
+    { name: "John F. Kennedy", terms: 1, party: "D" },
+    { name: "Lyndon Johnson", terms: 2, party: "D" },
+    { name: "Dwight Eisenhower", terms: 2, party: "R" }
+  ];
+   
+  //Challenge Questions
+  //1) Filter all presidents, leaving only the Democratic ones
+   /// use filter function
+  //2)Filter all presidents to leave only one term Republican presidents HINT use If statement
+   
+  //3) return only the last three presidents
+   
+  //4) log all dems who served 2 terms. HINT use chain filter, filter and slice
+   
+  //BONUS write a script to check if LBJ was a 2 term president. IF he was then alert("LBJ served two terms") else alert "LBJ was one and done"
