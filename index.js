ca1b69fc624da23754ae2a71af05ffc9210ee3e2
RonF25 <57496986+RonF25@users.noreply.github.com>
2020-04-02 13:57:41 -0400

    asdf

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 console.log("Connected");
+/*const Caroline = {
+    name: "Caroline",
+    dob: 2004
+    graduated: false,
+    age: function() {
+        //console.log(this);
+        return 2020 - this.dob;
+    }
+};*/
+
+/* const Homer = {
+    name: "Homer",
+    age:32
+    family: {
+        wife: {
+            name:"Marge"
+        }
+        son: {
+            name:"Bart"
+        }
+    },
+    pets: ["cat", "dog"]
+}; */
+
+const Haoran = {
+    name: "how",
+    dob: 2001,
+    year: "Senior",
+    age: function() {
+        //console.log(this);
+        return 2020 - this.dob;
+    }
+};
+
+/*const Eric = Haoran; /// refrence to Haoran DONT USE THIS
+Eric.name = "Eric" /// This makes Haorans name Eric
+const Christian = {};
+Christian.dob = "Haoran.dob" /// DO THIS
+Christian.dob = 2003;*/
+const calc = {
+    add: ///function x + y,
+    subtract: /// function x - y
+}
