e5b6b46fba79b6c07c43045bd00da8b0b10cc2cd
RonF25 <57496986+RonF25@users.noreply.github.com>
2020-04-07 13:59:11 -0400

    f

--- index.js
+++ index.js
@@ -92,3 +92,34 @@ Christian.dob = 2003;*/
 } */
 
 
+//const header = document.querySelector(".header")
+//const header = document.querySelector("#IdHeader")
+//const header = document.getElementById("Idheader")
+/* const items = document.querySelectorAll(".list-item")
+const itemsArr = Array.from(items);
+//items.forEach((element) => console.log(element)
+items.Arr.forEach((el) => (el.style.backgroundColor = "red")) */
+
+const header = document.querySelector(".header-two");
+const button = document.querySelector(".button")
+const background = document.querySelector(".background")
+///header.classList.add("Karen");
+///header.classList.remove("Karen")
+/* button.addEventListener("click", function(){
+    header.classList.toggle("Karen");
+}); */
+/* button.addEventListener("click", function(){
+    if(background.style.backgroundColor !== "rgb(255, 0, 0)"){
+        background.style.backgroundColor = "rgb(255, 0, 0)"
+    } else {
+        background.style.backgroundColor = "rgb(0, 0, 255)"
+    }
+}); */
+
+button.addEventListener("click", function(){
+    const R = Math.floor(Math.random()*256);
+    const G = Math.floor(Math.random()*256);
+    const B =Math.floor(Math.random()*256);
+    header.textcontent =`rgb(${R}, ${G}, ${B})`
+    background.style.backgroundColor = `rgb(${R}, ${G}, ${B})`;
+});
